287f4e8caa35b5104e910128d534a3b04181fc00
Jova <jovaandrea8721@gmail.com>
2021-08-10 03:36:40 +0700

    edit back to top btn

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,12 +1,15 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
+//Get the button
+var mybutton = document.getElementById("back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
+window.onscroll = function() {
+  var pageOffset = document.documentElement.scrollTop || document.body.scrollTop,
+  btn = document.getElementById('back-to-top');
+  if (btn) btn.style.visibility = pageOffset > 450 ? 'visible' : 'hidden';
+};
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
