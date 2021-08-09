8a30853d002c0b670cca6d571bd863b159a88107
Jova <jovaandrea8721@gmail.com>
2021-08-10 01:45:55 +0700

    add table news

---
+++ src/antihoax_web/database/factories/NewsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\News;
+use Faker\Generator as Faker;
+
+$factory->define(News::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
