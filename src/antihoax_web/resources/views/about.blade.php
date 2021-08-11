4763da296f2353dd321a9503646e8156d5a33e27
zaydzuhri <zaydzuhri@gmail.com>
2021-08-11 12:44:50 +0700

    Menambahkan frontend about.html ke Laravel

---
+++ src/antihoax_web/resources/views/about.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts/main')
+
+@section('container')
+	<div class="container-2">
+		<img src="img/antihoax.png">
+		<div class="box">
+			<p>"Truth! Truth! Truth!"</p>
+		</div>
+		<div class="content">
+			<p>
+				Kebenaran sangat penting bagi kami sampai kami mengulanginya 
+				tiga kali dalam motto kami. Di AntiHoax kami memimpikan 
+				suatu dunia dimana hoaks sepenuhnya dieradikasikan. Dunia 
+				dimana masyarakat umum dapat mengakses segala jenis informasi 
+				yang tersedia di internet tanpa adanya kekhawatiran bahwa 
+				informasi itu tidak valid.
+			</p>
+			<!-- <br> -->
+			<p>
+				AntiHoax menyediakan platform bagi para pengguna internet 
+				untuk melaporkan dan menilai kevalidan dari suatu laman 
+				berita sehingga masyarakat internet dapat lebih terinformasi 
+				atas halaman-halaman yang diragukan kebenarannya. 
+			</p>
+			<!-- <br> -->
+			<p>
+				Pelaporan hoaks dapat dilakukan oleh pengguna disertai dengan 
+				bukti bahwa link lampiran diragukan kebenarannya. Dari situ, 
+				pengguna-pengguna lain dapat menilai kebenaran atau ketidakbenaran 
+				dari laporan yang sudah ada.
+			</p>
+		</div>
+	</div>
+@endsection
