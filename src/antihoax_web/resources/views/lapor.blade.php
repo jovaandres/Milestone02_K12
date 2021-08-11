e7126d6dcdc9bc5c996b54a06b13ec0b0fecd128
zaydzuhri <zaydzuhri@gmail.com>
2021-08-11 12:45:09 +0700

    Menambahkan frontend lapor.html ke Laravel

---
+++ src/antihoax_web/resources/views/lapor.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts/main')
+
+@section('container')
+    <div class="container-2">
+        <h2>Lapor Hoaks</h2>
+        <div class="row">
+            <div class="column-1">
+                <form method="post">
+                    <label for="link">
+                        <h3>Link Berita</h3>
+                    </label>
+                    <input type="url" name="link" id="link" placeholder="Enter the link here">
+                    <label for="argument">
+                        <h3>Alasan</h3>
+                    </label>
+                    <textarea name="argument" class="text-area" placeholder="Enter the explanation here"></textarea>
+                </form>
+            </div>
+            <div class="column-2">
+                <div class="disclaimer-box">
+                    <h3>Disclaimer</h3>
+                    <p>Link berita yang dilaporkan pada laman web ini 
+                    tidak akan langsung dinyatakan sebagai hoaks. 
+                    Berita yang telah dilaporkan akan ditampilkan di 
+                    News List dan akan dikonfirmasi kebenarannya oleh 
+                    pengunjung-pengunjung laman web ini. Anda juga 
+                    dapat mengonfirmasi kebenaran berita lain pada laman 
+                    web ini di halaman news list</p>
+                </div>
+            </div>                
+        </div>
+        <center><button type="submit" id="submit-button" style="margin: auto;">Submit</button></center>
+    </div>    
+@endsection
